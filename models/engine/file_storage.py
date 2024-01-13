3d711669cb9740559bd64050ecf4a0373f02578c
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2024-01-13 04:02:32 -0800

    file_storage update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -18,34 +18,35 @@ class FileStorage:
 
     __file_path = "file.json"
     __objects = {}
+    classes = {"BaseModel": BaseModel, "User": User, "Place": Place,
+               "Amenity": Amenity, "City": City, "Review": Review,
+               "State": State}
 
     def all(self):
         """ returns the dictionary __objects """
-        return FileStorage.__objects
+        return self.__objects
 
     def new(self, obj):
         """ stores obj with key <obj class name>.id in __objects """
         key = "{}.{}".format(type(obj).__name__, obj.id)
-        FileStorage.__objects[key] = obj
+        self.__objects[key] = obj
 
     def save(self):
         """ serializes __objects to the JSON file """
         serialized_objects = {
                 key: value.to_dict()
-                for key, value in FileStorage.__objects.items()
+                for key, value in self.__objects.items()
         }
-        with open(FileStorage.__file_path, "w") as f:
+        with open(self.__file_path, "w") as f:
             json.dump(serialized_objects, f)
 
     def reload(self):
         """ deserializes the JSON file to __objects """
-        if os.path.exists(FileStorage.__file_path):
-            with open(FileStorage.__file_path, 'r') as f:
-                try:
-                    data = json.load(f)
-                    for key, obj_dict in data.items():
-                        class_name, obj_id = key.split('.')
-                        class_instance = eval(class_name)(**obj_dict)
-                        FileStorage.__objects[key] = class_instance
-                except Exception:
-                    pass
+        try:
+            with open(self.__file_path, 'r') as f:
+                data = json.load(f)
+            for key, value in data.items():
+                obj = self.classes[value['__class__']](**value)
+                self.__objects[key] = obj
+        except FileNotFoundError:
+            pass
